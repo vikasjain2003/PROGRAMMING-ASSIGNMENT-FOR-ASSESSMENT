3ada0513df60bb9ff321f1cd8a151c9780ecfa96
Vikas Jain <87235644+vikasjain2003@users.noreply.github.com>
2021-08-29 10:09:35 +0530

    Create Program1.cpp

---
+++ Program1.cpp
@@ -0,0 +1,13 @@
+// Program: 1
+// “Write a Program for use of predefined sizeof() function to display the data type”
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout<<"integer size is "<<sizeof(int)<<" bytes"<<endl;
+  cout<<"float size is "<<sizeof(float)<<" bytes"<<endl;
+  cout<<"char size is "<<sizeof(char)<<" bytes"<<endl;
+  cout<<"Double size is "<<sizeof(double)<<" bytes"<<endl;
+   return 0;
+}
