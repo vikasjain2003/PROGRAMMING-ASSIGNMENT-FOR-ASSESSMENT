8fc6fbb287c88098124fb69b8aece495cfecd80e
Vikas Jain <87235644+vikasjain2003@users.noreply.github.com>
2021-09-06 12:47:36 +0530

    Create Program9.cpp

---
+++ Program9.cpp
@@ -0,0 +1,13 @@
+// Program: 9
+// “Program for input two number and check whether it is Equal or not”
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a,b;
+  cout<<"enter the two numbers : ";
+  cin>>a>>b;
+  if(a == b) cout<<"Both numbers are equal"<<endl;
+  else cout<<"not equal"<<endl;
+   return 0;
+}
