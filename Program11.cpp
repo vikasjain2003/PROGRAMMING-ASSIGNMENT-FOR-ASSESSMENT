be3658f190f9a3cf598d119857a767bbba50cda4
Vikas Jain <87235644+vikasjain2003@users.noreply.github.com>
2021-09-06 13:20:38 +0530

    Create Program11.cpp

---
+++ Program11.cpp
@@ -0,0 +1,12 @@
+// Program: 11
+// “Write a Program to input a Number and Check whether it is even or odd”
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a;
+  cin>>a;
+  if(a%2 == 0) cout<<"Even number "<<endl;
+  else cout<<"Odd Number"<<endl;
+   return 0;
+}
